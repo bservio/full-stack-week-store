2ba7497e45da7736743e339a9efe5da12fe20c90
Bruno Sérvio Barbosa <brunoservio.dev@gmail.com>
2023-10-21 09:26:47 -0300

    feat: product-item and product-list components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,33 @@
-"use client";
 import Image from 'next/image'
 import Categories from './components/categories';
+import ProductList from './components/product-list';
+import { prismaClient } from '@/lib/prisma';
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      }
+    }
+  })
   return (
-    <div className="p-5">
+    <div>
       <Image 
         src="/banner-55off-home.png"
         width={0}
         height={0}
-        className='h-auto w-full'
+        className='h-auto w-fulla p-5'
         sizes="100vw"
         alt='até 55% de descontos, só esse mês'
       />
-      <div className="mt-8"></div>
-      <Categories />
+      <div className="mt-8 p-5">
+        <Categories />
+      </div>
+      <div className="mt-8">
+        <ProductList products={deals} />
+      </div>
+      
     </div>
   )
 }
