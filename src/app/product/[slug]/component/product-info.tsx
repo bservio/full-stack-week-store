2d6bee2592018af424e046fd6c8a031f0f21af4e
Bruno Sérvio Barbosa <brunoservio.dev@gmail.com>
2023-10-26 09:41:27 -0300

    feat: add product info component

--- src/app/product/[slug]/component/product-info.tsx
+++ src/app/product/[slug]/component/product-info.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/products";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, TruckIcon } from "lucide-react";
 import { useState } from "react";
 
 interface ProductInfoProps {
@@ -43,6 +43,32 @@ const ProductInfo = ({ product }: ProductInfoProps) => {
         <Button variant="outline" onClick={handleIncreaseButtonClick}>
           <ChevronRight size={14} />
         </Button>
+      </div>
+
+      <div className="mt-3 flex flex-col gap-3">
+        <h3 className="font-bold">Descrição</h3>
+        <p className="text-sm opacity-75">{product.description}</p>
+      </div>
+
+      <Button className="mt-8 font-bold uppercase">
+        Adicionar ao carrinho
+      </Button>
+
+      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2">
+        <div className="flex items-center gap-2">
+          <TruckIcon />
+
+          <div className="flex flex-col">
+            <p className="text-xs">
+              Entrega via <span className="font-bold">FSPacket®</span>
+            </p>
+            <p className="text-xs text-[#8162FF]">
+              Envio para <span className="font-bold">todo Brasil</span>
+            </p>
+          </div>
+        </div>
+
+        <p className="text-xs font-bold">Frete grátis</p>
       </div>
     </div>
   );
