a93fd8e14f42bf91d17a6e2eb234ab042c37482f
Bruno Servio Barbosa <bruno.servio@grupocev.int>
2023-10-26 15:45:40 -0300

    feat: Shopping cart context and component

---
+++ src/components/ui/cart.tsx
@@ -0,0 +1,27 @@
+import { CartContext } from "@/app/providers/cart";
+import { useContext } from "react";
+import { Badge } from "./badge";
+import { ShoppingCartIcon } from "lucide-react";
+
+const Cart = () => {
+  const { products } = useContext(CartContext);
+
+  return (
+    <div>
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        <ShoppingCartIcon size={16} />
+        Carrinho
+      </Badge>
+
+      {/* RENDERIZAR OS PRODUTOS */}
+      {products.map((product) => (
+        <h1 key={product.id}>{product.name}</h1>
+      ))}
+    </div>
+  );
+};
+
+export default Cart;
